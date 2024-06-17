da34a28a610ca7b757a4b83417a9c1a20d34e169
HristinaBuyanova <hristina-a@mail.ru>
2024-06-17 12:19:55 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Христина Буянова on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
