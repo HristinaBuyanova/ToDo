08fae76ffeac5c3143599eba8c8f3c1e3545d7b5
HristinaBuyanova <hristina-a@mail.ru>
2024-07-06 02:10:20 +0300

    Add CalendarTableViewHeader

---
+++ ToDo/Views/UIKit/CalendarTableViewHeader.swift
@@ -0,0 +1,45 @@
+
+import UIKit
+
+final class CalendarTableViewHeader: UITableViewHeaderFooterView {
+
+    static var identifier = "Header"
+
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 15, weight: .medium)
+        label.lineBreakMode = .byTruncatingTail
+        label.textColor = .labelSecondary
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .backPrimary
+        setupViews()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with title: String) {
+        titleLabel.text = title
+    }
+
+    private func setupViews() {
+        contentView.addSubview(titleLabel)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ])
+    }
+}
